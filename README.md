1450f99b9adc7dd20e1e9f25793ce0fcc038fddb
PRODIIF <125951820+michelstoc@users.noreply.github.com>
2023-06-05 17:12:19 +0200

    ok

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ClientAssistant
+# ClientAssistant# Hotel de la Sure Virtual Assistant
+
+Welcome to the GitHub repository of the Hotel de la Sure Virtual Assistant! This assistant uses OpenAI's language model to answer any questions you may have about our hotel, restaurant, spa, and surrounding area. It can be asked questions in multiple languages.
+
+## Requirements
+You will need to install some python libraries to be able to run this code, you can find these in the `requirements.txt` file at the root of this repository.
+
+## Installation
+You can install the required libraries by running:
+```
+pip install -r requirements.txt
+```
+
+## Usage
+To use this assistant, you need to launch the Streamlit app by running:
+```
+streamlit run app.py
+```
+Then, open your web browser and go to `http://localhost:8501` to start using the application.
+
+On the application, enter the usage code to access the question field. Type your question and hit enter, the AI will then process your question and display the answer.
+
--- app.py
+++ app.py
@@ -24,39 +24,52 @@ def main():
     st.set_page_config(page_title="Assistant de l'Hôtel de la Sûre")
     
     # st.image("logo_couleur.png")
-    st.header("Bienvenue sur notre assistant virtuel !")
-    st.write("Je suis ravi de vous aider à répondre à toutes vos questions concernant notre hôtel, notre restaurant, notre spa et les environs. Nous sommes ici pour vous offrir une expérience inoubliable et enrichissante")
-    st.write("Poser vos questions dans plusieurs langues. l'anglais, le français, le luxembourgoie, l'espagnol, l'allemand, l'italien, le néerlandais, le portugais, le danois, et encore d'autres...")
+    st.title("Bienvenue sur notre assistant virtuel !")
+    st.subheader("Je suis ravi de vous aider à répondre à toutes vos questions concernant notre hôtel, notre restaurant, notre spa et les environs. Nous sommes ici pour vous offrir une expérience inoubliable et enrichissante.")
     
-    # chargement data
-    text = lire_fichier_data()
-        
-    # split into chunks
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-        )
-    chunks = text_splitter.split_text(text)
+    st.write("""
+    Vous pouvez poser vos questions en plusieurs langues : anglais, français, luxembourgeois, espagnol, allemand, italien, néerlandais, portugais, danois et bien d'autres.
     
-    # create embeddings
-    embeddings = OpenAIEmbeddings()
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
+    Veuillez noter que ceci est une version de démonstration et un code d'accès est requis pour continuer.
+    """)
+    
+    # Ask for usage code
+    usage_code = st.text_input("Entrez votre code d'utilisation:")
+    if usage_code == 'Koekelberg1081':
         
-    # show user input
-    user_question = st.text_input("Votre question:")
-    if user_question:
-        with st.spinner("Rédaction de la réponse, veuillez patienter..."):
-            docs = knowledge_base.similarity_search(user_question)
+        
+        # chargement data
+        text = lire_fichier_data()
             
-            llm = OpenAI()
-            chain = load_qa_chain(llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=user_question)
-                print(cb)
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text)
+        
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+            
+        # show user input
+        user_question = st.text_input("Votre question:")
+        if user_question:
+            with st.spinner("Rédaction de la réponse, veuillez patienter..."):
+                docs = knowledge_base.similarity_search(user_question)
                 
-            st.write(response)
+                llm = OpenAI()
+                chain = load_qa_chain(llm, chain_type="stuff")
+                with get_openai_callback() as cb:
+                    response = chain.run(input_documents=docs, question=user_question)
+                    print(cb)
+                    
+                st.write(response)
+                
+    else :
+        st.write("Désolé, le code d'utilisation que vous avez entré n'est pas valide. Veuillez réessayer.")            
 
 
 if __name__ == '__main__':
